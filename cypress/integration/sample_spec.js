d0b3d0c93b24b6b77a5cbec13a11d2694fcb5c5e
hyeonhyejin <dev.hyejin04@gmail.com>
2020-03-11 00:13:15 +0900

    Add assertion
    Add assertion.

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,17 +1,23 @@
-describe('first test', ()=>{
-    it('success', ()=>{
-        expect(true).to.equal(true);
-    })
+describe("first test", () => {
+  it("success", () => {
+    expect(true).to.equal(true);
+  });
 
-    it('fail', ()=>{
-        expect(true).to.equal(false);
-    })
+  it("fail", () => {
+    expect(true).to.equal(false);
+  });
 });
 
+describe("visit a page", () => {
+  it("visit", () => {
+    cy.visit("https://example.cypress.io");
+    cy.contains("type").click();
 
-describe('visit a page', ()=>{
-    it('visit', ()=>{
-       cy.visit('https://example.cypress.io');
-       cy.contains('type');
-    })
+    cy.url().should("include", "/commands/actions");
+
+    //interacion
+    cy.get(".action-email")
+      .type("fake@email.com")
+      .should("have.value", "fake@email.com");
+  });
 });
